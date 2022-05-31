1808b68b865f6682a8c04269a6fd1c06d4dda028
jujimeizuo <3058852685@qq.com>
2022-05-31 14:35:46 +0800

    feat(func): Query extension and Reorder is done

--- py3/main.py
+++ py3/main.py
@@ -67,52 +67,6 @@ def upload_file(filename):
     cache.add(ImageModel(filename))
 
 
-# 检索图片
-def reduce_tmp():
-    start_time = time.time()
-    global cache
-    word_count = 1024
-
-    # 创建线程
-    class ThreadRunner(threading.Thread):
-        def __init__(self, function, index):
-            super().__init__()
-            self.function = function
-            self.index = index
-
-        def run(self) -> None:
-            self.function(self.index)
-
-        @staticmethod
-        def add(function, count):
-            thread_list = []
-            for i in range(count):
-                cur_thread = ThreadRunner(function, i)
-                cur_thread.start()
-                thread_list.append(cur_thread)
-            for thread in thread_list:
-                thread.join()
-
-    def get_des(index):
-        if cache.data[index].des is None:
-            cache.data[index].des = sift.get_des('./img/' + cache.data[index].image)
-
-    ThreadRunner.add(get_des, len(cache.data))
-    cache.word_list = sift.encode_plus([data.des for data in cache.data], word_count)
-    for img in cache.data:
-        img.word_summary = sift.get_word_summary(img.des, cache.word_list)
-    cache.idf = sift.tf_idf([data.word_summary for data in cache.data])
-    for img in cache.data:
-        img.idf_word_summary = sift.idf_render(img.word_summary, cache.idf)
-
-    cache.idf_count = len(cache.data)
-    end_time = time.time()
-    print("retrieve {} image cost: {}s".format(cache.idf_count, end_time - start_time))
-    with open(str(uuid.uuid4()), 'wb') as f:
-        pickle.dump(cache, f)  # 结果存储到文件
-    return Response.of_ok('OK').to_string()
-
-
 def reduce():
     global cache
     class_des = dict()
@@ -203,47 +157,5 @@ def find_image(filename):
                                     'p50_ac': "{:.2f}%".format(p50_ac_cnt / p50_cnt * 100),
                                     'p50_rc': "{:.2f}%".format(p50_ac_cnt / total_cnt * 100),
                                     'ap': ap_value / ap_cnt
-                                    },
-                           'data': [{'value': img[0], 'name': img[1]} for img in res[:img_count]]}).to_string()
-
-
-# 图像查找
-def find_image_tmp(filename):
-    img_count = 10
-    des = sift.get_des('./img/' + filename)
-    word_summary = sift.get_word_summary(des, cache.word_list)
-    idf_word_summary = sift.idf_render(word_summary, cache.idf)
-    res = []
-    image_label = filename.split('_')[0]
-    total_cnt = 0
-    top5_cnt = 0
-    top_10_cnt = 0
-    p50_cnt = 0
-    p50_ac_cnt = 0
-    for i in range(cache.idf_count):
-        match_value = sift.summary_match(idf_word_summary, cache.data[i].idf_word_summary)
-        if match_value > 0.5:
-            p50_cnt += 1
-            if cache.data[i].image.split('_')[0] == image_label:
-                total_cnt += 1
-                p50_ac_cnt += 1
-        elif cache.data[i].image.split('_')[0] == image_label:
-            total_cnt += 1
-        res.append((match_value, cache.data[i].image))
-    res.sort(key=lambda x: x[0], reverse=True)
-    for i in range(5):
-        if res[i][1].split('_')[0] == image_label:
-            top5_cnt += 1
-            top_10_cnt += 1
-    for i in range(5, 10):
-        if res[i][1].split('_')[0] == image_label:
-            top_10_cnt += 1
-
-    return Response.of_ok({'info': {'tp5_ac': "{:.2f}%".format(top5_cnt / 5 * 100),
-                                    'tp5_rc': "{:.2f}%".format(top5_cnt / total_cnt * 100),
-                                    'tp10_ac': "{:.2f}%".format(top_10_cnt / 10 * 100),
-                                    'tp10_rc': "{:.2f}%".format(top_10_cnt / total_cnt * 100),
-                                    'p50_ac': "{:.2f}%".format(p50_ac_cnt / p50_cnt * 100),
-                                    'p50_rc': "{:.2f}%".format(p50_ac_cnt / total_cnt * 100)
                                     },
                            'data': [{'value': img[0], 'name': img[1]} for img in res[:img_count]]}).to_string()
